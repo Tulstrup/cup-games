17a1a987c2624ff7ad164421146ffa15a69fd01f
Mathias Prisfeldt <lopri@mobilepay.dk>
2021-04-14 18:15:30 +0200

    update

--- games/balloon/src/scenes/main-scene.ts
+++ games/balloon/src/scenes/main-scene.ts
@@ -5,79 +5,86 @@ import { Gameworld } from '../interfaces/gameworld.interface';
 import { Dart } from '../objects/dart';
 
 export class MainScene extends Phaser.Scene implements Gameworld {
-  slingshot: Slingshot;
+	slingshot: Slingshot;
 
-  dartGroup: Phaser.GameObjects.Group;
-  balloonGroup: Phaser.GameObjects.Group;
+	dartGroup: Phaser.GameObjects.Group;
+	balloonGroup: Phaser.GameObjects.Group;
 
-  constructor() {
-    super({ key: 'MainScene' });
-  }
+	constructor() {
+		super({ key: 'MainScene' });
+	}
 
-  preload(): void {
-    this.load.image('dart', 'images/dart.png');
-    this.load.image('balloon', 'images/balloon.png');
-  }
+	preload(): void {
+		this.load.image('dart', 'images/dart.png');
+		this.load.image('balloon', 'images/balloon.png');
+		this.load.image('hand', 'images/hand.png');
+	}
 
-  create(): void {
-    this.slingshot = new Slingshot({
-      scene: this,
-      gameworld: this,
-      position: new Phaser.Math.Vector2(
-        this.scale.width * CONST.SLINGSHOT_POSITION_PERCENTAGE.x,
-        this.scale.height * CONST.SLINGSHOT_POSITION_PERCENTAGE.y
-      )
-    });
+	create(): void {
+		this.slingshot = new Slingshot({
+			scene: this,
+			gameworld: this,
+			position: new Phaser.Math.Vector2(
+				this.scale.width * CONST.SLINGSHOT_POSITION_PERCENTAGE.x,
+				this.scale.height * CONST.SLINGSHOT_POSITION_PERCENTAGE.y
+			)
+		});
 
-    this.dartGroup = this.add.group({
-      runChildUpdate: true
-    });
+		this.dartGroup = this.add.group({
+			runChildUpdate: true
+		});
 
-    this.balloonGroup = this.add.group({
-      runChildUpdate: true
-    });
+		this.balloonGroup = this.add.group({
+			runChildUpdate: true
+		});
 
-    this.spawnBalloons();
-  }
+		this.spawnBalloons();
+	}
 
-  update(total: number, deltaTime: number): void {
-    this.slingshot.update(deltaTime);
+	update(total: number, deltaTime: number): void {
+		this.slingshot.update(deltaTime);
 
-    this.physics.overlap(
-      this.dartGroup,
-      this.balloonGroup,
-      (dart, balloon) => {
-        this.dartGroup.remove(dart, true, true);
-        this.balloonGroup.remove(balloon, true, true);
-      }
-    );
-  }
+		this.physics.overlap(
+			this.dartGroup,
+			this.balloonGroup,
+			(dart, balloon) => {
+				this.dartGroup.remove(dart, true, true);
+				this.balloonGroup.remove(balloon, true, true);
+			}
+		);
+	}
 
-  spawnDart(position: Phaser.Math.Vector2, direction: Phaser.Math.Vector2) {
-    const newDart = new Dart({
-      scene: this,
-      position: position,
-      direction: direction
-    });
+	spawnDart(position: Phaser.Math.Vector2, direction: Phaser.Math.Vector2) {
+		const newDart = new Dart({
+			scene: this,
+			position: position,
+			direction: direction
+		});
 
-    this.dartGroup.add(newDart);
-  }
+		this.dartGroup.add(newDart);
+	}
 
-  spawnBalloons(): void {
-    for (let i = 0; i < CONST.BALLOON.TOTAL; i++) {
-      const balloon = new Balloon({
-        scene: this,
-        x: this.getRandomSpawnPostion(1, CONST.BALLOON.TOTAL * CONST.BALLOON.SIZE),
-        y: this.getRandomSpawnPostion(CONST.BALLOON.MIN_Y_POSITION, CONST.BALLOON.MAX_Y_POSITION),
-        texture: 'balloon',
-        hasGift: false
-    });
+	spawnBalloons(): void {
+		for (let i = 0; i < CONST.BALLOON.TOTAL; i++) {
+			const balloon = new Balloon({
+				scene: this,
+				x: this.getRandomSpawnPostion(
+					1,
+					CONST.BALLOON.TOTAL * CONST.BALLOON.SIZE
+				),
+				y: this.getRandomSpawnPostion(
+					CONST.BALLOON.MIN_Y_POSITION,
+					CONST.BALLOON.MAX_Y_POSITION
+				),
+				texture: 'balloon',
+				hasGift: false
+			});
 
-    this.balloonGroup.add(balloon);
-  }
-}
+			this.balloonGroup.add(balloon);
+		}
+	}
 
 	private getRandomSpawnPostion(min: number, max: number): number {
-  return Phaser.Math.RND.between(min, max);
-}
+		return Phaser.Math.RND.between(min, max);
+	}
 }
