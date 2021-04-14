6ac8638a7664a11395e76f729b2963187e401b5c
Jonas Tulstrup <jonjo@danskebank.dk>
2021-04-14 08:40:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+Team CUP games made in Phaser with the purpose of being interactable wrappings for Gifts
+
+# Games
+- npm run template
---
+++ games/jonas/src/config.ts
@@ -0,0 +1,18 @@
+import { MainScene } from './scenes/main-scene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Jonas Playground',
+  version: '1.0',
+  width: 800,
+  height: 600,
+  backgroundColor: 0x3a404d,
+  type: Phaser.AUTO,
+  parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: [MainScene]
+};
