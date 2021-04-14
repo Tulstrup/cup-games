40d2acb418d8d288c3074b3f5f44c894241cb67f
Khawaja Farooq Ahmed <khah@mobilepay.dk>
2021-04-14 16:47:20 +0200

    cleanup

--- games/balloon/src/objects/balloon.ts
+++ games/balloon/src/objects/balloon.ts
@@ -6,7 +6,7 @@ interface BalloonParameters {
     y: number;
     texture: string | Phaser.Textures.Texture;
     frame?: string | number;
-  }
+}
 
 export class Balloon extends Phaser.GameObjects.Sprite {
 
@@ -18,17 +18,25 @@ export class Balloon extends Phaser.GameObjects.Sprite {
     constructor(params: BalloonParameters) {
         super(params.scene, params.x, params.y, params.texture)
 
-        this.displayWidth = 140;
-		this.displayHeight = 140;
+        this.displayWidth = CONST.BALLOON.SIZE;
+        this.displayHeight = CONST.BALLOON.SIZE;
         this.radius = 0;
 
-        // physics
+        this.applyPhysics();
+
+        this.scene.add.existing(this);
+    }
+
+    update(): void {
+        this.applyVelocity();
+        this.checkIfOffScreen();
+    }
+
+    private applyPhysics(): void {
         this.scene.physics.world.enable(this);
         this.body.allowGravity = false;
         this.body.setCircle(this.radius);
         this.body.setOffset(-this.radius, -this.radius);
-
-        this.scene.add.existing(this);
 
         this.velocity = this.getRandomVelocity(
             CONST.BALLOON.MINSPEED,
@@ -36,48 +44,43 @@ export class Balloon extends Phaser.GameObjects.Sprite {
         );
     }
 
-    update(): void {
-        this.applyVelocity();
-        this.checkIfOffScreen();
-      }
-
     private applyVelocity(): void {
         this.x += this.velocity.x;
         this.y += this.velocity.y;
-      }
+    }
 
-      private checkIfOffScreen(): void {
+    private checkIfOffScreen(): void {
         // horizontal check
-        if (this.x > this.scene.sys.canvas.width + 140) {
-          this.x = -CONST.BALLOON.SIZE;
+        if (this.x > this.scene.sys.canvas.width + CONST.BALLOON.SIZE) {
+            this.x = -CONST.BALLOON.SIZE;
         } else if (this.x < -CONST.BALLOON.SIZE) {
-          this.x = this.scene.sys.canvas.width + CONST.BALLOON.SIZE;
+            this.x = this.scene.sys.canvas.width + CONST.BALLOON.SIZE;
         }
-    
+
         // vertical check
         if (this.y > this.scene.sys.canvas.height + CONST.BALLOON.SIZE) {
-          this.y = -CONST.BALLOON.SIZE;
+            this.y = -CONST.BALLOON.SIZE;
         } else if (this.y < -CONST.BALLOON.SIZE) {
-          this.y = this.scene.sys.canvas.height + CONST.BALLOON.SIZE;
+            this.y = this.scene.sys.canvas.height + CONST.BALLOON.SIZE;
         }
-      }
+    }
 
     private getRandomVelocity(aMin: number, aMax: number): Phaser.Math.Vector2 {
         return new Phaser.Math.Vector2(
-          Phaser.Math.RND.between(
-            this.getRndNumber(aMin, aMax),
-            this.getRndNumber(aMin, aMax)
-          ),
-          Phaser.Math.RND.between(
-            this.getRndNumber(aMin, aMax),
-            this.getRndNumber(aMin, aMax)
-          )
+            Phaser.Math.RND.between(
+                this.getRndNumber(aMin, aMax),
+                this.getRndNumber(aMin, aMax)
+            ),
+            Phaser.Math.RND.between(
+                this.getRndNumber(aMin, aMax),
+                this.getRndNumber(aMin, aMax)
+            )
         );
-      }
-    
-      private getRndNumber(aMin: number, aMax: number): number {
+    }
+
+    private getRndNumber(aMin: number, aMax: number): number {
         let num = Math.floor(Math.random() * aMax) + aMin;
         num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
         return num;
-      }
+    }
 }
--- games/balloon/src/scenes/main-scene.ts
+++ games/balloon/src/scenes/main-scene.ts
@@ -5,47 +5,45 @@ import { Slingshot } from '../objects/slingshot';
 export class MainScene extends Phaser.Scene {
 
   private balloons: Balloon[];
-	private slingshot: Slingshot;
+  private slingshot: Slingshot;
 
-	constructor() {
-		super({ key: 'MainScene' });
-	}
+  constructor() {
+    super({ key: 'MainScene' });
+  }
 
   preload(): void {
     this.load.image('dart', 'images/dart.png');
     this.load.image('balloon', 'images/balloon.png');
   }
 
-	create(): void {
-		this.slingshot = new Slingshot({
-			scene: this,
-			position: new Phaser.Math.Vector2(
-				this.scale.width / 2,
-				this.scale.height * 0.9
-			)
-		});
+  create(): void {
+    this.slingshot = new Slingshot({
+      scene: this,
+      position: new Phaser.Math.Vector2(
+        this.scale.width / 2,
+        this.scale.height * 0.9
+      )
+    });
 
     this.spawnBalloons();
-	}
+  }
 
-	update(total: number, deltaTime: number): void {
-		this.slingshot.update(deltaTime);
+  update(total: number, deltaTime: number): void {
+    this.slingshot.update(deltaTime);
 
     for (let i = 0; i < CONST.BALLOON.TOTAL; i++) {
       this.balloons[i].update();
     }
-	}
+  }
 
   spawnBalloons(): void {
     this.balloons = [];
     
-    const maximumXPosition = this.sys.canvas.width - CONST.BALLOON.MIN_X_POSITION * 2
-
     for (let i = 0; i < CONST.BALLOON.TOTAL; i++) {
       this.balloons.push(
         new Balloon({
           scene: this,
-          x: this.getRandomSpawnPostion(1, CONST.BALLOON.TOTAL) * 140,
+          x: this.getRandomSpawnPostion(1, CONST.BALLOON.TOTAL) * CONST.BALLOON.SIZE,
           y: this.getRandomSpawnPostion(CONST.BALLOON.MIN_Y_POSITION, CONST.BALLOON.MAX_Y_POSITION),
           texture: 'balloon'
         })
@@ -53,15 +51,7 @@ export class MainScene extends Phaser.Scene {
     }
   }
 
-  // balloons shouldn't overlap
   private getRandomSpawnPostion(min: number, max: number): number {
     return Phaser.Math.RND.between(min, max);
-    //return Phaser.Math.RND.between(1, CONST.BALLOON_COUNT) * 140;
-    //x, y + 140 x 140
   }
-
-  // private getRandomPosition(): Phaser.Math.Vector2 {
-  //   const position = Phaser.Math.RND.between(1, CONST.BALLOON_COUNT);
-  //   position+140
-  // }
 }
